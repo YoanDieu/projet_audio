b42eb432d2b269142005f55e17932fc1da885dd8
YoanDieu <yoan.dieu@gmail.com>
2015-08-20 20:05:57 +0200

    fixed nav, svg file ==> white, add main with home as class in home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,5 +56,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  get 'pages/home' => "pages#home"
+  root 'pages#home'
 end
